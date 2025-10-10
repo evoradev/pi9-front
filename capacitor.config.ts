a3aa4e33aec52dde0d6841ee66a0d1db87ef9b84
LuanLucasGon <lucasluangon@gmail.com>
2025-10-10 07:46:52 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pi4-front',
+  webDir: 'www'
+};
+
+export default config;
